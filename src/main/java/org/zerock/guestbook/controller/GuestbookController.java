da9ee444b033077992b6e7acc8c3ec9917471240
eune2507 <uforu3@gmail.com>
2024-01-17 11:07:01 +0900

    1/17 교재 181페이지

--- src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -5,7 +5,11 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.dto.PageRequestDTO;
 import org.zerock.guestbook.service.GuestbookService;
 
@@ -24,5 +28,17 @@ public class GuestbookController {
     public void list(PageRequestDTO pageRequestDTO, Model model){
         log.info("list........" + pageRequestDTO);
         model.addAttribute("result",service.getList(pageRequestDTO));
+    }
+    //------------------------------------------------------------------------
+    @GetMapping("/register")
+    public void register(){
+        log.info("register...");
+    }
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+        log.info("dto..." + dto);
+        Long gno = service.register(dto);
+        redirectAttributes.addFlashAttribute("msg", gno);
+        return "redirect:/guestbook/list";
     }
 }
